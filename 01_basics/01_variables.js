38b61f2377918c12f973f0f400351e5c2d1a4ef5
Shantanu2066 <119879613+Shantanu2066@users.noreply.github.com>
2024-06-15 10:56:34 +0000

    control config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accID = 1226373
+let accEmail = 'shantanughosh105@gmail.com'
+var accPass = 'chaicode7789'
+
+accCity = "Kolkata" //we can create a variable without let, var or const also but it's not recommended
+let accState //We can create a variable without assigning anything. It's stored as 'undefined' in JS
+
+console.table([accID,accEmail, accPass,accCity, accState])
+
+/*
+Prefer not to use var because of issue in blockscope and functional scope
+*/

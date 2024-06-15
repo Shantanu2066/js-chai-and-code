610095ba5411244a4ce21c6f9272d7fc2ca5f434
Shantanu2066 <119879613+Shantanu2066@users.noreply.github.com>
2024-06-15 11:59:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+let name = "shantanu" //string
+let age = 22 //number
+let isLoggedIn = true //boolean
+let state = null
+let country
+
+/* Primitive Data Types
+string
+numbers
+bigint
+boolean
+null 
+undefined
+symbol
+
+Non Primitive or Complex Data Type:
+Object
+
+*/
+
+// To find data type in JS, we can use typeof operator
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof country)
+console.log(typeof state)
+
+
+//even though null is a primitive data type, typeof null shows object. This is a bug in JavaScript Language
